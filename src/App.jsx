8addf26217faea09ede596c53961febe461122d1
Scott Cousins <scousins1024@yahoo.com>
2023-10-19 13:17:48 -0500

    working code with home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Routes, Route, Link} from "react-router-dom"
 import Blue from './components/Blue'
 import Red from './components/Red'
+import Home from './components/Home'
 
 function App() {
 
@@ -9,6 +10,8 @@ function App() {
     <div id="container">
       <div id="navbar">{
         <div>
+          <Link to='/'>Home</Link>
+          <br/>
           <Link to='/blue'>Blue</Link>
           <br/>
           <Link to='/red'>Red</Link>
@@ -16,6 +19,7 @@ function App() {
       }</div>
       <div id="main-section">
         <Routes>
+          <Route path='/' element={<Home />} />
           <Route path='/blue' element={<Blue />} />
           <Route path='/red' element={<Red />} />
         </Routes>
