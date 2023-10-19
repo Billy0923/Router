51d91a39fe7ec1d400fd2420fda4673f4d6c3af7
Scott Cousins <scousins1024@yahoo.com>
2023-10-19 12:46:57 -0500

    Routes were corrected to render page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route, Link} from "react-router-dom"
+import Blue from './components/Blue'
+import Red from './components/Red'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div id="container">
-      <div id="navbar">{/* navigation here */}</div>
+      <div id="navbar">{
+        <div>
+          <Link to='/blue'>Blue</Link>
+          <br/>
+          <Link to='/red'>Red</Link>
+        </div>
+      }</div>
       <div id="main-section">
         <Routes>
-          <Route path = '/blue'  element= { <h1 className='blue'>blue</h1>}/> 
-          <Route path = '/red' element = { <h1 className='red'>red</h1>} /> 
+          <Route path='/blue' element={<Blue />} />
+          <Route path='/red' element={<Red />} />
         </Routes>
       </div>
     </div>
